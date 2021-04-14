1650e75b103641d1f2e71e7ed769b205eaca4f8b
jkwadraat <jan.janssen@thomasmore.be>
2021-04-14 14:42:29 +0200

    aanpassing replaceWords example van String object pagina

--- src/es6/strings/replaceWords.js
+++ src/es6/strings/replaceWords.js
@@ -1,13 +1,17 @@
 const original = document.getElementById('original').innerHTML;
 
-// replace only the first word!
+// Replace only the first word (=default) - case sensitive (=default)
 const replace1 = document.getElementById('replace1');
 replace1.innerHTML = original.replace('Python', 'JavaScript').replace('programming', 'scripting');
 
-// replace globally but case sensitive
+// Replace only the first word (=default) - case insensitive
 const replace2 = document.getElementById('replace2');
-replace2.innerHTML = original.replace(/Python/g, 'JavaScript').replace('programming', 'scripting');
+replace2.innerHTML = original.replace(/Python/i, 'JavaScript').replace(/programming/i, 'scripting');
 
-// replace globally but case sensitive
+// Replace globally - case sensitive (=default)
 const replace3 = document.getElementById('replace3');
-replace3.innerHTML = original.replace(/PYTHON/gi, 'JavaScript').replace('programming', 'scripting');
+replace3.innerHTML = original.replace(/Python/g, 'JavaScript').replace(/programming/g, 'scripting');
+
+// Replace globally - case insensitive
+const replace4 = document.getElementById('replace4');
+replace4.innerHTML = original.replace(/Python/gi, 'JavaScript').replace(/programming/gi, 'scripting');
