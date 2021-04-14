3c6e1615c350cef3068fb75e37035609cd17f72b
jkwadraat <jan.janssen@thomasmore.be>
2021-04-14 14:57:09 +0200

    aanpassing afronding in cylinder example van Math object pagina

--- src/es6/math/cylinder.js
+++ src/es6/math/cylinder.js
@@ -5,10 +5,13 @@ document.querySelectorAll('[type=number]').forEach(function (input) {
         const volume = radius * radius * Math.PI * height;
 
         document.querySelector('#volume1 b').innerHTML = `${volume} cm<sup>2</sup>`;
+
         // rounded
         document.querySelector('#volume2 b').innerHTML = `${Math.round(volume)} cm<sup>2</sup>`;
-        //rounded to 2 decimal points
-        document.querySelector('#volume3 b').innerHTML = `${Math.round(volume * 100) / 100} cm<sup>2</sup>`;
+
+        // rounded to 2 decimal points
+        // document.querySelector('#volume3 b').innerHTML = `${Math.round(volume * 100) / 100} cm<sup>2</sup>`;
+        document.querySelector('#volume3 b').innerHTML = `${volume.toFixed(2)} cm<sup>2</sup>`;
     });
 });
 
