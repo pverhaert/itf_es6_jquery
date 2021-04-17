31cfeb16408a87d22f962427f1a1455e499b6f10
jkwadraat <jan.janssen@thomasmore.be>
2021-04-17 10:48:57 +0200

    aanpassing cm³ ipv cm² in cylinder volume example op Math object gina -

--- src/es6/math/cylinder.js
+++ src/es6/math/cylinder.js
@@ -4,14 +4,14 @@ document.querySelectorAll('[type=number]').forEach(function (input) {
         const height = document.getElementById('height').value;
         const volume = radius * radius * Math.PI * height;
 
-        document.querySelector('#volume1 b').innerHTML = `${volume} cm<sup>2</sup>`;
+        document.querySelector('#volume1 b').innerHTML = `${volume} cm<sup>3</sup>`;
 
         // rounded
-        document.querySelector('#volume2 b').innerHTML = `${Math.round(volume)} cm<sup>2</sup>`;
+        document.querySelector('#volume2 b').innerHTML = `${Math.round(volume)} cm<sup>3</sup>`;
 
         // rounded to 2 decimal points
-        // document.querySelector('#volume3 b').innerHTML = `${Math.round(volume * 100) / 100} cm<sup>2</sup>`;
-        document.querySelector('#volume3 b').innerHTML = `${volume.toFixed(2)} cm<sup>2</sup>`;
+        // document.querySelector('#volume3 b').innerHTML = `${Math.round(volume * 100) / 100} cm<sup>3</sup>`;
+        document.querySelector('#volume3 b').innerHTML = `${volume.toFixed(2)} cm<sup>3</sup>`;
     });
 });
 
