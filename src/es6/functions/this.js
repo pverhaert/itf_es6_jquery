d6cc8343ab8f920b1f6f9ede426e44d2a4004dcd
jkwadraat <jan.janssen@thomasmore.be>
2021-04-18 15:39:27 +0200

    overbodige code in this.js (arrow functions) verwijderd

--- src/es6/functions/this.js
+++ src/es6/functions/this.js
@@ -11,14 +11,3 @@ secondaryBtn.addEventListener('click', (e) => {
     console.log('this inside arrow function', this);
     console.log('e.target', e.target);
 });
-
-// user1 is an object literal
-const user1 = {
-    // properties
-    name: 'John',
-    surname: 'Doe',
-    // functions (or methodes)
-    greet() {
-        console.log(`Hi, ${this.name} ${this.surname}`);
-    },
-};
