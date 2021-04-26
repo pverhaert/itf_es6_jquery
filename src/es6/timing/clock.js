2ee2e65ba60fe1fdc032abc922c066b7c0cf41d9
jkwadraat <jan.janssen@thomasmore.be>
2021-04-26 13:09:20 +0200

    clock-example: added (more) comments

--- src/es6/timing/clock.js
+++ src/es6/timing/clock.js
@@ -1,13 +1,19 @@
 function clock() {
+    // make new Date-object containing the current date/time
     const now = new Date();
+
+    // DIGITAL clock
+    // select the (text) element for the digital clock and give it the correct content
+    // now.toLocaleTimeString('en-GB') converts now to HH:MM:SS format
+    document.getElementById('digitalTime').textContent = now.toLocaleTimeString('en-GB');
+
+    // ANALOG clock
+    // get seconds, minutes and hours (0-12) from the object now
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours() % 12; // convert 24 hours to 12 hours
 
-    // digital clock
-    document.getElementById('digitalTime').textContent = now.toLocaleTimeString();
-
-    // analog clock
+    // select the (line) elements for the hands of the analog clock and rotate them according to the values of seconds, minutes and hours
     // 1 second (360/60 = 6 degrees)
     document.getElementById('sec').setAttribute('transform', `rotate(${seconds * 6}, 150, 150)`);
     // 1 minute (= 6 degrees)
@@ -17,4 +23,4 @@ function clock() {
 }
 
 clock(); // start clock() immediately
-setInterval(clock, 1000); // run clock() every second
+setInterval(clock, 1000); // execute clock() repeatedly (every second = 1000ms)
