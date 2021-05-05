a0b88a45d6c3ab3b7ffddb91b1d8447f7cfc105c
Patrick Verhaert <patrick.verhaert@thomasmore.be>
2021-05-05 16:16:28 +0200

    Update jquery

--- src/jquery/effects/effects.js
+++
@@ -1,9 +0,0 @@
-$('#effects').change(function () {
-    const select = $(this).find(':selected').text();
-    eval(select);
-    // if (select.includes('toggle')) {
-    //     $(this).val(-1);
-    // }
-});
-
-$('#reset').click(() => $('figure').attr('style', ''));
--- src/jquery/traversing/tree_traversal.js
+++
@@ -1,7 +0,0 @@
-$('#travers')
-    .change(function () {
-        const select = $(this).find(':selected').text();
-        $('*').removeClass('selected');
-        eval(select).addClass('selected');
-    })
-    .change();
