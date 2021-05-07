59e9c023274ee6839640e3ac0b0dfbcfc721b858
Patrick Verhaert <patrick.verhaert@thomasmore.be>
2021-05-07 10:38:34 +0200

    jQuery DOM traversing

--- src/jquery/traversing/rating.js
+++ src/jquery/traversing/rating.js
@@ -1,25 +1,39 @@
+// click on a star
 $('figcaption span').click(function () {
+    // remove the class .rating on all it's siblings
     $(this).siblings().removeClass('rated');
+    // add the class .rating to all the previous siblings and on itself
     $(this).prevAll().addBack().addClass('rated');
+    // move up the DOM to the closest figure tag and from there, move down the DOM and find the image tag
+    // add the class .border to the image
     $(this).closest('figure').find('img').addClass('border');
     countStars();
 });
 
+// click on an image
 $('figure img').click(function () {
+    // move up the DOM to the closest figure tag and from there, move down the DOM and find all span tags
+    // remove the class .rated from all the span tags
     $(this).closest('figure').find('span').removeClass('rated');
+    // remove the class .border from the image
     $(this).removeClass('border');
     countStars();
 });
 
+// clear the total score
 $('#clearScore').click(function () {
+    // remove the class .rated from all the spans
     $('span').removeClass('rated');
+    // remove the class .border on all the images
     $('img').removeClass('border');
-    // $('*').removeClass('border rated');
+    // remark: the two lines above can be writhe in just one line: $('*').removeClass('border rated');
     countStars();
 });
 
 function countStars() {
+    // Count all stars witch has the class .rating
     const total = $('.rated').length;
     $('#counter').text(total);
-    $('#clearScore').prop('disabled', !(total > 0));
+    // disable the button if total is 0
+    $('#clearScore').prop('disabled', total === 0);
 }
--- src/jquery/traversing/rotateImages.js
+++ src/jquery/traversing/rotateImages.js
@@ -1,13 +1,23 @@
+// rotate images
 $('#rotate').on('input', function () {
     const value = $(this).val();
+    // even images: rotate with value
     $('main img').even().css('transform', `rotate(${value}deg)`);
+    // odd images: rotate with -value
     $('main img').odd().css('transform', `rotate(${-value}deg)`);
 });
 
+// count the images and update the max value for the #shadow slider
+// add a drop-shadow to the selected image
+// trigger the input event on the #shadow slider
 $('#shadow')
+    .attr('max', $('main img').length)
     .on('input', function () {
         const value = $(this).val();
+        // remove the drop-shadow from all images
         $('main img').css('filter', 'none');
+        // add drop-shadow to the selected image
+        // remark: eq(x) starts from index 0 while our slider starts from 1
         $('main img')
             .eq(value - 1)
             .css('filter', `drop-shadow(0 0 20px rgba(0, 0, 0, 1)`);
