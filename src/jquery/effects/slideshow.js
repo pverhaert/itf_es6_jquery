811647df40aa13e9472fb2fb4d8a96628bd8aa3c
Patrick Verhaert <patrick.verhaert@thomasmore.be>
2021-05-17 08:11:28 +0200

    jQuery slideshow toegevoegd

---
+++ src/jquery/effects/slideshow.js
@@ -0,0 +1,19 @@
+const idMin = 151;
+const idMax = 180;
+let currentImg = 151;
+
+function loop() {
+    let nextImg = currentImg;
+    // the next image is a random image between idMin and idMax but may not be the same as the current image
+    do {
+        nextImg = Math.floor(Math.random() * (idMax - idMin + 1) + idMin);
+    } while (nextImg === currentImg);
+    currentImg = nextImg;
+    // figure slideUp() -> replace the image -> wait 500ms -> slideDown()
+    $('figure').slideUp(1000, function () {
+        $(this).empty().append(`<img src="https://picsum.photos/id/${currentImg}/600/400">`).delay(500).slideDown(1000);
+    });
+}
+
+loop();
+setInterval(loop, 4000);
